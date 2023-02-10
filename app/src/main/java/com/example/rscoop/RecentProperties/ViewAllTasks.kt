9b901a8bf84924f11256535480648e96d447a46d
rahul1478 <65287732+rahul1478@users.noreply.github.com>
2023-02-11 00:54:43 +0530

    searchBar implemented in view all task

--- app/src/main/java/com/example/rscoop/RecentProperties/ViewAllTasks.kt
+++ app/src/main/java/com/example/rscoop/RecentProperties/ViewAllTasks.kt
@@ -2,7 +2,10 @@ package com.example.rscoop.RecentProperties
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,10 +22,12 @@ import retrofit2.Response
 class ViewAllTasks : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+
     private lateinit var recentrecycler: RecentRecycler
 
     lateinit var shimmerFrameLayout: ShimmerFrameLayout
     lateinit var shimmerLayout: LinearLayout
+    private lateinit var search:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +36,9 @@ class ViewAllTasks : AppCompatActivity() {
         recyclerView = findViewById(R.id.recycler_Tasks)
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        //search
+        search = findViewById(R.id.search_Bar)
 
         shimmerFrameLayout = findViewById(R.id.shimmer_tasks_view_container)
         shimmerLayout = findViewById(R.id.shimmer_layout_tasks)
@@ -53,6 +61,28 @@ class ViewAllTasks : AppCompatActivity() {
 
                 shimmerLayout.setVisibility(View.GONE)
                 recyclerView.visibility = View.VISIBLE
+
+                search.addTextChangedListener(object :TextWatcher{
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                    }
+
+                    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                        val originalData = response.toList()
+                        val filterData = originalData.filter { item ->
+                            item.hotel_name.contains(p0.toString(),ignoreCase = true)
+                        }
+
+                        recentrecycler.updateData(filterData)
+                    }
+
+                    override fun afterTextChanged(p0: Editable?) {
+
+                    }
+
+                })
+
             }
             override fun onFailure(call: Call<List<TaskData>?>, t: Throwable) {
                 Toast.makeText(this@ViewAllTasks,t.localizedMessage,Toast.LENGTH_LONG)
