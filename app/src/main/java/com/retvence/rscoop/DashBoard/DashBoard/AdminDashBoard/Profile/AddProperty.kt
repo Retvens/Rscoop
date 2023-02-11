299c4034eb77feaa8de75523c201fc22ae32f41b
Arjun Gupta <arjungupta0817@gmail.com>
2023-02-12 03:08:37 +0530

    add property, calendar, ...

---
+++ app/src/main/java/com/retvence/rscoop/DashBoard/DashBoard/AdminDashBoard/Profile/AddProperty.kt
@@ -0,0 +1,12 @@
+package com.retvence.rscoop.DashBoard.DashBoard.AdminDashBoard.Profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.retvens.rscoop.R
+
+class AddProperty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_property)
+    }
+}
