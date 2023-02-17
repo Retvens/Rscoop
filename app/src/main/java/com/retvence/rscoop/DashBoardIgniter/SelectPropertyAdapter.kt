b9f93d0b87235482d32d1cbcfb1a817e9beb69f2
Arjun Gupta <arjungupta0817@gmail.com>
2023-02-17 20:20:17 +0530

    properties with selection

--- app/src/main/java/com/retvence/rscoop/DashBoardIgniter/SelectPropertyAdapter.kt
+++ app/src/main/java/com/retvence/rscoop/DashBoardIgniter/SelectPropertyAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
@@ -14,10 +16,14 @@ import com.retvens.rscoop.R
 
 class SelectPropertyAdapter(val context : Context, var items: List<HotelsData>) : RecyclerView.Adapter<SelectPropertyAdapter.SelectPropertyViewHolder>() {
 
+
+    var adapterPosition = -1
+
     class SelectPropertyViewHolder(itemView: View) : ViewHolder(itemView){
         val imageHotel = itemView.findViewById<ImageView>(R.id.hotel_add_task_img)
         val nameHotel = itemView.findViewById<TextView>(R.id.hotel_name_add_task)
         val starHotel = itemView.findViewById<TextView>(R.id.star_rate_number)
+        val relativeLayout = itemView.findViewById<RelativeLayout>(R.id.selectPropertyRelative)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SelectPropertyViewHolder {
@@ -35,8 +41,20 @@ class SelectPropertyAdapter(val context : Context, var items: List<HotelsData>) 
         holder.nameHotel.text = items.hotel_name
         holder.starHotel.text = items.hotel_stars
         Glide.with(context).load(items.Cover_photo).into(holder.imageHotel)
-    }
 
+        if (adapterPosition == position){
+            holder.relativeLayout.background = context.getDrawable(R.drawable.rectangle_outline)
+        }else{
+            holder.relativeLayout.background = context.getDrawable(R.drawable.rectangle_background)
+        }
+
+        holder.itemView.setOnClickListener {
+            adapterPosition = position
+            notifyDataSetChanged()
+            Toast.makeText(holder.itemView.context, items.hotel_name,Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
     fun updateData(newData : List<HotelsData>){
         items = newData
         notifyDataSetChanged()
