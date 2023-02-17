03fa3153dadbc6433399d9345c80aeb63199c931
Arjun Gupta <arjungupta0817@gmail.com>
2023-02-17 16:14:44 +0530

    Calendar Toast With selected date

--- app/src/main/java/com/retvence/rscoop/RecentProperties/CalendarDateModel.kt
+++ app/src/main/java/com/retvence/rscoop/RecentProperties/CalendarDateModel.kt
@@ -8,6 +8,9 @@ data class  CalendarDateModel(var data: Date, var isSelected: Boolean = false) {
     val calendarDay: String
         get() = SimpleDateFormat("EE", Locale.getDefault()).format(data)
 
+    val calendarYear: String
+        get() = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(data)
+
     val calendarDate: String
         get() {
             val cal = Calendar.getInstance()
