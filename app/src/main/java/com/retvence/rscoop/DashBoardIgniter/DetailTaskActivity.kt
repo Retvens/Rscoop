69558089a8c849b48dab182369720f5decc340d4
Arjun Gupta <arjungupta0817@gmail.com>
2023-02-18 15:41:52 +0530

    Two New Activities Added

--- app/src/main/java/com/retvence/rscoop/DashBoardIgniter/DetailTaskActivity.kt
+++ app/src/main/java/com/retvence/rscoop/DashBoardIgniter/DetailTaskActivity.kt
@@ -1,17 +1,23 @@
 package com.retvence.rscoop.DashBoardIgniter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import com.retvens.rscoop.R
 
 class DetailTaskActivity : AppCompatActivity() {
 
+    lateinit var edit : CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_task)
 
-
+        edit = findViewById(R.id.edit_card)
+        edit.setOnClickListener {
+            startActivity(Intent(this@DetailTaskActivity,DetailTaskEditActivity::class.java))
+        }
 
     }
 }
---
+++ app/src/main/java/com/retvence/rscoop/DashBoardIgniter/DetailTaskEditActivity.kt
@@ -0,0 +1,12 @@
+package com.retvence.rscoop.DashBoardIgniter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.retvens.rscoop.R
+
+class DetailTaskEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_task_edit)
+    }
+}
