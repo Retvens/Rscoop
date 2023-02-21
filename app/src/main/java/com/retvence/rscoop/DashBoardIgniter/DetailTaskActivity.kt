9877cf7f413d24393cc291128cdd1ef93b5ae17e
Arjun Gupta <arjungupta0817@gmail.com>
2023-02-21 13:14:47 +0530

    Alignment Fixed

--- app/src/main/java/com/retvence/rscoop/DashBoardIgniter/DetailTaskActivity.kt
+++ app/src/main/java/com/retvence/rscoop/DashBoardIgniter/DetailTaskActivity.kt
@@ -7,16 +7,43 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
 import com.bumptech.glide.Glide
+import com.retvence.rscoop.RecentProperties.CalendarAdapter
+import com.retvence.rscoop.RecentProperties.CalendarDateModel
 import com.retvens.rscoop.R
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DetailTaskActivity : AppCompatActivity() {
+
+    private lateinit var dtDateMonth: TextView
+    private lateinit var dtCalendarNext: ImageView
+    private lateinit var dtCalendarPrevious: ImageView
+
+    private val sdf = SimpleDateFormat("MMMM yyyy", Locale.ENGLISH)
+    private val cal = Calendar.getInstance(Locale.ENGLISH)
+    private val dates = ArrayList<Date>()
+    private val calendarList2 = ArrayList<CalendarDateModel>()
+    private val currentDate = Calendar.getInstance(Locale.ENGLISH)
+    lateinit var calendarAdapter: CalendarAdapter
+
+    private lateinit var recyclerViewDate: RecyclerView
+
 
     lateinit var edit : CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_task)
+
+        dtCalendarPrevious = findViewById(R.id.dt_calendar_previous)
+        dtCalendarNext = findViewById(R.id.dt_calendar_next)
+        dtDateMonth = findViewById(R.id.dt_date_month)
+        recyclerViewDate = findViewById(R.id.recyclerViewDateDetail)
 
         edit = findViewById(R.id.edit_card)
 
@@ -68,6 +95,59 @@ class DetailTaskActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
-
+        setUpClickListener()
+        setUpAdapter()  // First We Set Adapter
+        setUpCalendar() // Now Set Calendar
     }
+
+    private fun setUpClickListener() {
+        dtCalendarNext.setOnClickListener {
+            cal.add(Calendar.MONTH, 1)
+            setUpCalendar()
+        }
+        dtCalendarPrevious.setOnClickListener {
+            cal.add(Calendar.MONTH, -1)
+            if (cal == currentDate)
+                setUpCalendar()
+            else
+                setUpCalendar()
+        }
+    }
+    /**
+     * Setting up adapter for recyclerview
+     */
+    private fun setUpAdapter() {
+        val snapHelper: SnapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerViewDate)
+        calendarAdapter = CalendarAdapter { calendarDateModel: CalendarDateModel, position: Int ->
+            calendarList2.forEachIndexed { index, calendarModel ->
+                calendarModel.isSelected = index == position
+            }
+
+            calendarAdapter.setData(calendarList2)
+        }
+        recyclerViewDate.adapter = calendarAdapter
+    }
+
+    /**
+     * Function to setup calendar for every month
+     */
+    private fun setUpCalendar() {
+        val calendarList = ArrayList<CalendarDateModel>()
+        dtDateMonth.text = sdf.format(cal.time)
+        val monthCalendar = cal.clone() as Calendar
+        val maxDaysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH)
+        dates.clear()
+        monthCalendar.set(Calendar.DAY_OF_MONTH, 1)
+        while (dates.size < maxDaysInMonth) {
+            dates.add(monthCalendar.time)
+            calendarList.add(CalendarDateModel(monthCalendar.time))
+            monthCalendar.add(Calendar.DAY_OF_MONTH, 1)
+        }
+        calendarList2.clear()
+        calendarList2.addAll(calendarList)
+        calendarAdapter.setData(calendarList)
+    }
+
+
 }
