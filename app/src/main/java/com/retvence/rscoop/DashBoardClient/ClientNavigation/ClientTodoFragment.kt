3f377202ed8c356e4bc57be6857b035e3b1f66d3
Arjun Gupta <arjungupta0817@gmail.com>
2023-02-25 03:37:43 +0530

    Added Listeners

--- app/src/main/java/com/retvence/rscoop/DashBoardClient/ClientNavigation/ClientTodoFragment.kt
+++ app/src/main/java/com/retvence/rscoop/DashBoardClient/ClientNavigation/ClientTodoFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.facebook.shimmer.ShimmerFrameLayout
@@ -13,9 +14,12 @@ import com.retvence.rscoop.ApiRequests.RetrofitBuilder
 import com.retvence.rscoop.DashBoardClient.ClientTaskAdapter
 import com.retvence.rscoop.DataCollections.GetTaskData
 import com.retvens.rscoop.R
+import dev.joshhalvorson.calendar_date_range_picker.calendar.CalendarPicker
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.DateFormat
+import java.util.*
 
 class ClientTodoFragment : Fragment() {
 
@@ -24,6 +28,8 @@ class ClientTodoFragment : Fragment() {
     private lateinit var clientTaskAdapter: ClientTaskAdapter
     lateinit var shimmerFrameLayout: ShimmerFrameLayout
 
+    private lateinit var calendarPicker: CalendarPicker
+    private lateinit var get: CardView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +42,20 @@ class ClientTodoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        calendarPicker = view.findViewById(R.id.calendarPicker)
+        get = view.findViewById(R.id.get_calendar)
+
+        get.setOnClickListener{
+
+        val selectedDates = calendarPicker.getSelectedDates()
+
+        if (selectedDates != null) {
+            val firstDate = DateFormat.getDateInstance().format(Date(selectedDates.first))
+            val secondDate = DateFormat.getDateInstance().format(Date(selectedDates.second))
+            Toast.makeText(context,firstDate.toString() + " to " + secondDate.toString(),Toast.LENGTH_SHORT).show()
+          }
+
+        }
 
         recyclerView = view.findViewById(R.id.recycler_Tasks_todo_client)
         recyclerView.setHasFixedSize(true)
@@ -62,7 +82,7 @@ class ClientTodoFragment : Fragment() {
                     recyclerView.visibility = View.VISIBLE
 
                 }else{
-                    Toast.makeText(context,response.code(), Toast.LENGTH_SHORT)
+                    Toast.makeText(context,response.code().toString(), Toast.LENGTH_SHORT)
                         .show()
                 }
             }
