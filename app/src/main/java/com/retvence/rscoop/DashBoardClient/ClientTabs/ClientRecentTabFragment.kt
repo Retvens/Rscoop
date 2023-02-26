be159a4f82bb85ae4bf14e0739935af6c28f70d1
Arjun Gupta <arjungupta0817@gmail.com>
2023-02-26 23:01:16 +0530

    Recent Tab Done

--- app/src/main/java/com/retvence/rscoop/DashBoardClient/ClientTabs/ClientRecentTabFragment.kt
+++ app/src/main/java/com/retvence/rscoop/DashBoardClient/ClientTabs/ClientRecentTabFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.ShimmerFrameLayout
 import com.retvence.rscoop.ApiRequests.RetrofitBuilder
 import com.retvence.rscoop.DashBoardClient.ClientTaskAdapter
 import com.retvence.rscoop.DashBoardIgniter.TaskFragment.RecentAdapter
@@ -24,6 +25,7 @@ class ClientRecentTabFragment : Fragment() {
     private lateinit var owner_id: String
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var shimmerFrameLayout: ShimmerFrameLayout
     private lateinit var clientTaskAdapter:ClientTaskAdapter
 
     override fun onCreateView(
@@ -39,6 +41,7 @@ class ClientRecentTabFragment : Fragment() {
 
         owner_id = SharedPreferenceManagerAdmin.getInstance(context!!).user.owner_id.toString()
 
+        shimmerFrameLayout = view.findViewById(R.id.shimmer_tasks_r_container)
 
         //recycler
         recyclerView = view.findViewById(R.id.client_recent_tab_tasks)
@@ -56,13 +59,14 @@ class ClientRecentTabFragment : Fragment() {
                 response: Response<List<GetTaskData>?>
             ) {
 
+                shimmerFrameLayout.stopShimmer()
+                shimmerFrameLayout.visibility = View.GONE
+
                 val response = response.body()!!
                 if (response != null && view != null) {
                     clientTaskAdapter = ClientTaskAdapter(context!!, response)
                     clientTaskAdapter.notifyDataSetChanged()
                     recyclerView.adapter = clientTaskAdapter
-                }else{
-                    Toast.makeText(context,response.toString(),Toast.LENGTH_SHORT).show()
                 }
             }
             override fun onFailure(call: Call<List<GetTaskData>?>, t: Throwable) {
--- app/src/main/java/com/retvence/rscoop/DashBoardClient/ClientTaskAdapter.kt
+++ app/src/main/java/com/retvence/rscoop/DashBoardClient/ClientTaskAdapter.kt
@@ -8,19 +8,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
+import com.retvence.rscoop.ApiRequests.RetrofitBuilder
 import com.retvence.rscoop.DataCollections.GetTaskData
+import com.retvence.rscoop.DataCollections.ResponseTask
 import com.retvens.rscoop.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ClientTaskAdapter(val context: Context, var userList:List<GetTaskData>) : RecyclerView.Adapter<ClientTaskAdapter.ClientTaskViewHolder>() {
 
+    var adapterPosition = -1
+    var favourite = false
     class ClientTaskViewHolder(itemView: View) : ViewHolder(itemView){
 
         var name = itemView.findViewById<TextView>(R.id.Company_Name)
         var image = itemView.findViewById<ImageView>(R.id.owner_Image)
         var favorite = itemView.findViewById<ImageView>(R.id.favorite_icon)
+        var status = itemView.findViewById<ImageView>(R.id.status_Image)
+        var location = itemView.findViewById<TextView>(R.id.hotel_task)
+        var date = itemView.findViewById<TextView>(R.id.hotel_date)
 
         var facebook = itemView.findViewById<TextView>(R.id.facebooktask)
         var instagram = itemView.findViewById<TextView>(R.id.instatask)
@@ -49,6 +60,30 @@ class ClientTaskAdapter(val context: Context, var userList:List<GetTaskData>) : 
     }
 
     override fun onBindViewHolder(holder: ClientTaskViewHolder, position: Int) {
+
+        if(adapterPosition == position){
+            holder.favorite.setColorFilter(context.getResources().getColor(R.color.sky_blue))
+        }else{
+            holder.favorite.setColorFilter(context.getResources().getColor(R.color.light_grey))
+        }
+        if (userList[position].Status == "Done"){
+            holder.status.setImageResource(R.drawable.png_check)
+        }else{
+            holder.status.setImageResource(R.drawable.png_timer)
+        }
+
+        if (userList[position].favourite == true) {
+            holder.favorite.setColorFilter(context.getResources().getColor(R.color.sky_blue))
+        }
+
+        holder.favorite.setOnClickListener {
+            adapterPosition = position
+            notifyDataSetChanged()
+            favourite = true
+            val id = userList[position]._id
+            setFavourite(id)
+        }
+
         holder.facebook.text = userList[position].facebook
         holder.instagram.text = userList[position].instagram
         holder.linkdin.text = userList[position].Linkedin
@@ -58,6 +93,9 @@ class ClientTaskAdapter(val context: Context, var userList:List<GetTaskData>) : 
         holder.google.text = userList[position].Google_reviews
         Glide.with(context).load(userList[position].owner_pic).into(holder.image)
         holder.name.text = userList[position].hotel_name
+        holder.date.text = userList[position].Date
+
+
 
         holder.favorite.visibility = View.VISIBLE
 
@@ -74,7 +112,6 @@ class ClientTaskAdapter(val context: Context, var userList:List<GetTaskData>) : 
             intent.putExtra("pinterest",userList[position].Pinterest)
             context.startActivity(intent)
         }
-
         holder.fbIcon.setOnClickListener{
             val uriForFB : Uri = Uri.parse("https://facebook.com")
             val openFB = Intent(Intent.ACTION_VIEW,uriForFB)
@@ -110,5 +147,23 @@ class ClientTaskAdapter(val context: Context, var userList:List<GetTaskData>) : 
             context.startActivity(openGoogle)
         }
 
+    }
+
+    private fun setFavourite(id: String) {
+        val fav = RetrofitBuilder.retrofitBuilder.setFav(id, FavouriteDataClass(favourite))
+        fav.enqueue(object : Callback<ResponseTask?> {
+            override fun onResponse(call: Call<ResponseTask?>, response: Response<ResponseTask?>) {
+                if (response.isSuccessful){
+                    val response = response.body()!!
+                    Toast.makeText(context,response.message.toString(), Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(context,response.code().toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseTask?>, t: Throwable) {
+                Toast.makeText(context,t.localizedMessage!!.toString(), Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
